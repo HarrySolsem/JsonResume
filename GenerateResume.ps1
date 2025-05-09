af9b8ac4ab6b456111e32edc58cbb1756a77969c
Harry Solsem <harry@solsem.no>
2025-05-09 13:53:49 +0200

    debug with default values in pwsh script

--- GenerateResume.ps1
+++ GenerateResume.ps1
@@ -17,11 +17,11 @@ param (
     
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    [string]$logFile = ".\dynamic_creation.log",
+    [string]$logFile = "dynamic_creation.log",
     
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    [string]$configFile = ".\config.json",
+    [string]$configFile = "config.json",
     
     [Parameter(Mandatory = $false)]
     [int]$jsonDepth = 5
